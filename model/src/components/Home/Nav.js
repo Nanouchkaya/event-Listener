00338321e2411d7c116215da56f1d120b37d82cf
Nanouchkaya <brun.claudine@live.fr>
2019-10-16 15:59:02 +0200

    menu burger ok (sauf animation)

--- model/src/components/Home/Nav.js
+++ model/src/components/Home/Nav.js
@@ -1,50 +1,71 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import { FaBars } from 'react-icons/fa';
 
 
-// == Composant
-const Nav = () => (
-  <nav className="navigation-item">
-    <NavLink
-      to="/"
-      exact
-      className="navigation-item--left"
-    >
-      eventListener
-    </NavLink>
+class Nav extends React.Component {
+  state = {
+    searchBar: false,
+    menuBurger: false,
+  }
 
-    <div>
-      <NavLink
-        to="/events"
-        exact
-        activeClassName="navigation-item--active"
-        className="navigation-item--right"
-      >
-        Tous les événements
-      </NavLink>
+  activeMenu = () => {
+    const bool = this.state.menuBurger ? false : true;
+    this.setState({
+      menuBurger: bool,
+    });
+  }
 
-      <NavLink
-        to="/register"
-        exact
-        activeClassName="navigation-item--active"
-      >
-        <button type="button" className="navigation-item--right button">
-          Inscription
-        </button>
-      </NavLink>
+  render() {
+    const { menuBurger, searchBar } = this.state;
+    // const searchBarClass = searchBar ? 'searchbar-visible' : 'searchbar';
+    const menuBurgerClass = menuBurger ? 'menu-visible' : 'menu';
+    return (
+      <nav className="navigation-item">
+        <NavLink
+          to="/"
+          exact
+          className="navigation-item--left"
+        >
+          eventListener
+        </NavLink>
 
-      <NavLink
-        to="/login"
-        exact
-        activeClassName="navigation-item--active"
-      >
-        <button type="button" className="navigation-item--right button">
-          Connexion
-        </button>
-      </NavLink>
-    </div>
-  </nav>
-);
+        <FaBars className="menu-burger" onClick={this.activeMenu} />
+
+        <div className={menuBurgerClass}>
+          <NavLink
+            to="/events"
+            exact
+            activeClassName="navigation-item--active"
+            className="navigation-item--right"
+          >
+            Tous les événements
+          </NavLink>
+
+          <NavLink
+            to="/register"
+            exact
+            activeClassName="navigation-item--active"
+          >
+            <button type="button" className="navigation-item--right button">
+              Inscription
+            </button>
+          </NavLink>
+
+          <NavLink
+            to="/login"
+            exact
+            activeClassName="navigation-item--active"
+          >
+            <button type="button" className="navigation-item--right button">
+              Connexion
+            </button>
+          </NavLink>
+        </div>
+      </nav>
+    );
+  }
+}
 
 // == Export
 export default Nav;
