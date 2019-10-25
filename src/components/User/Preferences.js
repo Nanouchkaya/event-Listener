8195dd3b55e24cc4becd04c9084ca5b076e0e297
Ines Mouandjo Lobe <inesmouandjolobe@gmail.com>
2019-10-25 09:41:04 +0200

    save for git pull

--- src/components/User/Preferences.js
+++ src/components/User/Preferences.js
@@ -1,12 +1,14 @@
-// import npm
-import React, { useState } from 'react';
+// == Import : npm
+import React from 'react';
 import PropTypes from 'prop-types';
 
-// import local
+
+// == Import : local
 import './user.scss';
 import DeleteAccount from 'src/components/User/DeleteAccount';
 
-// composant
+
+// == Composant Preferences
 const Preferences = ({
   firstname,
   lastname,
@@ -15,29 +17,33 @@ const Preferences = ({
   confirmePassword,
   notifNewEvent,
   notifNewUpdate,
+  changeCheckInputValue,
   changeValue,
-  changeCheckValue,
+  modalStatus,
+  changeModalStatus,
+  editorModeDisabled,
+  handleChangEditorMode,
 }) => {
-  // Hook for display/hidden the modal
-  const [modalStatus, changeModalStatus] = useState(false);
-  // Hook for display/hidden editor mode
-  const [editorModeDisabled, changEditorModeDisabled] = useState(true);
-
+  /* Passade de true/false pour affichage des modals */
   const handleChangeModalStatus = () => {
-    changeModalStatus(!modalStatus);
+    changeModalStatus();
   };
 
-  const handleChangEditorModeDisabled = () => {
-    changEditorModeDisabled(!editorModeDisabled);
+  const handleChangEditorModeDisabled = (event) => {
+    event.preventDefault();
+    handleChangEditorMode();
   };
 
+  /* Permet de récupérer la valeur entrée dans l'input */
   const handleChangeValue = (event) => {
     const { value, name } = event.target;
-    changeValue(value, name);
+    changeValue(name, value);
   };
+
+  /* Passage de true/false sur les checkbox */
   const handleChangeCheckValue = (event) => {
     const { name } = event.target;
-    changeCheckValue(name);
+    changeCheckInputValue(name);
   };
 
   return (
@@ -45,11 +51,8 @@ const Preferences = ({
       <h2 className="preferences-title">
         Mes informations
       </h2>
-
       <form className="preferences-infos">
-
         <div className="preferences-infos-container">
-
           <div className="content content--left">
 
             <label
@@ -134,7 +137,6 @@ const Preferences = ({
           </div>
 
           <div className="content content--right">
-
             <div className="content-input-checkbox">
               <input
                 className="checkbox"
@@ -143,7 +145,7 @@ const Preferences = ({
                 type="checkbox"
                 disabled={editorModeDisabled}
                 checked={notifNewEvent}
-                onChange={handleChangeCheckValue}
+                onClick={handleChangeCheckValue}
               />
               <label
                 className="label"
@@ -206,6 +208,9 @@ const Preferences = ({
     </section>
   );
 };
+
+
+// == PropTypes
 Preferences.propTypes = {
   firstname: PropTypes.string.isRequired,
   lastname: PropTypes.string.isRequired,
@@ -215,7 +220,13 @@ Preferences.propTypes = {
   notifNewEvent: PropTypes.bool.isRequired,
   notifNewUpdate: PropTypes.bool.isRequired,
   changeValue: PropTypes.func.isRequired,
-  changeCheckValue: PropTypes.func.isRequired,
+  changeCheckInputValue: PropTypes.func.isRequired,
+  modalStatus: PropTypes.bool.isRequired,
+  changeModalStatus: PropTypes.func.isRequired,
+  editorModeDisabled: PropTypes.bool.isRequired,
+  handleChangEditorMode: PropTypes.func.isRequired,
 };
-// export
+
+
+// == Export
 export default Preferences;
