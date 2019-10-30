8341cee9d2315ed1f72074f89e3d52275b8e8d14
Guillaume-TR <guillaume.terrien.gt@gmail.com>
2019-10-30 11:29:32 +0100

    config: remove & rename env. variables

--- app/index.js
+++ app/index.js
@@ -1,6 +1,5 @@
 const config = require('dotenv').config(); // Add config to env node.js
 if (config.error) throw config.error; // Check if there is no error
-// console.log('Config :', config.parsed); // Check config
 
 const bodyParser = require('body-parser');
 const router = require('express').Router();
@@ -20,4 +19,4 @@ server.use(router);
 App(server, router);
 
 // Defined a port for the server, if this constant is undefined, the server take the port 3000 
-server.listen(process.env.SERVER_PORT || 3000);
+server.listen(process.env.PORT || 3000);
